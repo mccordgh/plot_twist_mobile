7784007b248390c8342e8f0c339c2b3c1dbe6ad0
Matthew McCord <mattm.eternal@gmail.com>
2019-01-07 20:46:25 -0800

    Create a game constants object

---
+++ src/app/constants/game-constants.js
@@ -0,0 +1,14 @@
+export default {
+    FONT_SIZE: 32,
+
+    FPS: 60,
+
+    GAME_HEIGHT: 768,
+    GAME_WIDTH: 768,
+
+    SPRITE_HEIGHT: 64,
+    SPRITE_WIDTH: 64,
+
+    TILE_HEIGHT: 64,
+    TILE_WIDTH: 64,
+}
