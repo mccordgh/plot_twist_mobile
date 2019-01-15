284f06922fc19719ae8c536ff7257eac131e2742
Matthew McCord <mattm.eternal@gmail.com>
2019-01-14 21:07:46 -0800

    Initialize world with a couple entities

--- src/app/classes/worlds/world.js
+++ src/app/classes/worlds/world.js
@@ -1,9 +1,26 @@
+
+import { FarmHouse } from '../entities/static-entities/farm-house';
+import gameConstants from '../../constants/game-constants';
+import { Garden } from '../entities/static-entities/garden';
+
 export class World {
+    constructor(handler) {
+        this.handler = handler;
+        this.entityManager = handler.createEntityManager();
+
+        this.loadEntities();
+    }
+
     tick(deltaTime) {
-        //
+        this.entityManager.tick(deltaTime);
     }
 
     render(graphics) {
-        graphics.drawText('WOrld Hello!', 200, 200);
+        this.entityManager.render(graphics);
+    }
+
+    loadEntities() {
+        this.entityManager.addEntity(new FarmHouse(this.handler, 0, 275));
+        this.entityManager.addEntity(new Garden(this.handler, 101, 275));
     }
 }
