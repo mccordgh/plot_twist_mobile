418b0f6bed6f03627549b3c8d92d87130f9e82d2
Matthew McCord <mattm.eternal@gmail.com>
2019-01-14 21:06:04 -0800

    Create asset related classes

---
+++ src/app/classes/assets/assets.js
@@ -0,0 +1,24 @@
+import { SpriteSheet } from './sprite-sheet';
+import { ImageLoader } from "./image-loader";
+
+import gameConstants from '../../constants/game-constants';
+
+const assets = {};
+
+export class Assets {
+    constructor (name, path) {
+        assets[name] = this;
+        this.name = name;
+        this.path = path;
+        // this.height = gameConstants.SPRITE_HEIGHT
+        // this.width = gameConstants.SPRITE_WIDTH;
+        this.sheet = new SpriteSheet(ImageLoader.loadImage(this.path));
+    }
+
+    static getAssets(name) {
+        return assets[name];
+    }
+}
+
+let cursor = new Assets('cursor', `${gameConstants.BASE_PATH}src/resources/cursor.png`);
+cursor.pointer = cursor.sheet.crop(0, 0, 28, 32);
