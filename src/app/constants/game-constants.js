2967a6df8743378dc7c1359a11ccdb7849a0f717
Matthew McCord <mattm.eternal@gmail.com>
2019-01-21 22:19:12 -0800

    Update constants to include entity types

--- src/app/constants/game-constants.js
+++ src/app/constants/game-constants.js
@@ -3,14 +3,24 @@ export default {
 
     FPS: 60,
 
-    GAME_HEIGHT: 768,
-    GAME_WIDTH: 768,
+    // Iphone 7 in Landscape mode
+    GAME_HEIGHT: 551,
+    GAME_WIDTH: 980,
 
-    SPRITE_HEIGHT: 64,
-    SPRITE_WIDTH: 64,
+    SPRITE_HEIGHT: 80,
+    SPRITE_WIDTH: 80,
 
-    TILE_HEIGHT: 64,
-    TILE_WIDTH: 64,
+    CREATURE_SPEED: 100,
+
+    SPATIAL_GRID_SIZE: 96,
+
+    TYPES: {
+        MONSTER: 'monster',
+        HOUSE: 'house',
+        LANE: 'lane',
+    },
+
+    rndIndex: (arr) => arr[Math.floor(Math.random() * (arr.length))],
 
     BASE_PATH: window.location.href,
 }
